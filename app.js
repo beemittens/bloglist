e32687a5f735b8feb12909d418e77adff34df10c
beemittens <lauri.i.soini@gmail.com>
2020-05-18 11:42:24 +0300

    fix to routerr name

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
-const notesRouter = require('./controllers/blogs')
+const blogRouter = require('./controllers/blogs')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -22,7 +22,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
-app.use('/api/blogs', notesRouter)
+app.use('/api/blogs', blogRouter)
 
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
